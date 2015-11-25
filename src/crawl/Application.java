35f0ab4e2b7d0e952c2b82409d96a8b0a071eb38
727656494@qq.com <727656494>
2015-11-25 12:34:12 +0800

    init

---
+++ src/crawl/Application.java
@@ -0,0 +1,53 @@
+package crawl;
+
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+/*				Rule rule = new Rule("http://news.baidu.com/ns",  
+			            new String[] { "word" }, new String[] { "支付宝" },  
+			            null, -1, Rule.GET);  
+			    List<LinkTypeData> extracts = ExtractService.extract(rule);  
+			    printf(extracts); */
+			    
+		 /*Rule rule2 = new Rule(  
+	                "http://www1.sxcredit.gov.cn/public/infocomquery.do?method=publicIndexQuery",  
+	        new String[] { "query.enterprisename","query.registationnumber" }, new String[] { "兴网","" },  
+	                "cont_right", Rule.CLASS, Rule.POST);  
+	        List<LinkTypeData> extracts2 = ExtractService.extract(rule2);  
+	        printf(extracts2);  */
+		
+		Rule rule = new Rule("http://jingyan.baidu.com/article/ca41422fc76c4a1eae99ed9f.html",  
+	            new String[] {}, new String[] {},  
+	            null, -1, Rule.GET);  
+	    List<LinkTypeData> extracts = ExtractService.extract(rule);  
+	    printf(extracts); 
+	    }  
+
+	    public void getDatasByCssQuery()  
+	    {  
+	        Rule rule = new Rule("http://www.11315.com/search",  
+	                new String[] { "name" }, new String[] { "兴网" },  
+	                "div.g-mn div.con-model", Rule.SELECTION, Rule.GET);  
+	        List<LinkTypeData> extracts = ExtractService.extract(rule);  
+	        printf(extracts);  
+	    }  
+	  
+	   
+		
+
+
+public static void printf(List<LinkTypeData> datas)  
+{  
+    for (LinkTypeData data : datas)  
+    {  
+        System.out.println(data.getLinkText());  
+        System.out.println(data.getLinkHref());  
+        System.out.println("***********************************");  
+    }  
+
+}  
+
+}
